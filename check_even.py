25cc2492045830300eebf2c8edb0654e5cbe3d57
lucashernandezv <lucashernandezv@gmail.com>
2017-05-07 22:15:51 -0300

    Create check_even.py

---
+++ check_even.py
@@ -0,0 +1,10 @@
+def check_even():
+  num1=int(input("Type any number: "))
+  if num1 % 2 == 0:
+    print(num1,"is even.")
+  else:
+    print(num1,"is odd.")
+check_even()
+
+#def check_even(num1):
+#  return num1%2==0
