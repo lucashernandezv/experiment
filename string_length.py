c05f442a232c248ab88b6e4e1190dc9fe455c732
lucashernandezv <lucashernandezv@gmail.com>
2017-05-07 22:35:39 -0300

    Create string_length.py

---
+++ string_length.py
@@ -0,0 +1,17 @@
+#def string_length(myString):
+#  count = 0
+#  for letter in myString:
+#    count+=1
+    
+#  return count
+
+#print((string_length("lucas")))
+
+def string_length1():
+  myString=input("Type anything:")
+  count = 0
+  for letter in myString:
+    count+=1
+  print(count)
+  
+string_length1()
