80b550f598c87e11921b555d0159076f73b6364b
Lucas Hernández <lucashernandezv@users.noreply.github.com>
2017-05-07 23:50:07 -0300

    Create rock_paper_scissors.py
    so far only computer:rock vs user:rock,paper,scissors.
    
    left to do:
    computer:paper vs user:rock
    computer:paper vs user:paper
    computer:paper vs user:scissors
    
    computer:scissors vs user:rock
    computer:scissors vs user:paper
    computer:scissors vs user:scissors

---
+++ rock_paper_scissors.py
@@ -0,0 +1,65 @@
+import random
+
+class player():
+  def __init__(self, name):
+    self.name=name
+    
+# CREATES PLAYER 1
+print("[Player 1]")
+player1=player(input("Type in your name:"))
+
+# DETERMINES COMPUTER MOVE
+computerMove = random.randint(1,3)
+
+if computerMove=="1":
+  computerHand="Rock"
+elif computerMove=="2":
+  computerHand="Paper"
+else:
+  computerHand="Scissors"
+  
+# USER MOVE
+def gameMenu():
+  print("1. Rock")
+  print("2. Paper")
+  print("3. Scissors")
+ 
+choose=True
+while choose:
+  gameMenu()
+
+  choice=input("Type in your option: ")
+  if choice == "1":
+    userHand="Rock"
+    print("Rock")
+    choose=False
+  elif choice == "2":
+    userHand="Paper"
+    print("Paper")
+    choose=False
+  elif choice == "3":
+    userHand="Scissors"
+    print("Scissors")
+    choose=False
+  else:
+    print("Wrong option. Try again.")
+    print("")
+
+def battle():
+  # COMPUTER ROCK VS USER ROCK
+  if computerMove=="1" and choice=="1":
+    print("Computer:",computerHand)
+    print(player1.name,":",userHand)
+    print(computerHand,"v/s",userHand,". Draw.")
+  # COMPUTER ROCK VS USER PAPER
+  elif computerMove=="1" and choice=="2":
+    print("Computer:",computerHand)
+    print(player1.name,":",userHand)
+    print(userHand,"beats",computerHand)
+  # COMPUTER ROCK VS USER SCISSORS
+  elif computerMove=="1" and choice=="3":
+    print("Computer:",computerHand)
+    print(player1.name,":",userHand)
+    print(computerHand,"beats",userHand)
+
+battle()
